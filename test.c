b0909cfe1d5b700b6450c898cd28e93f612be0cf
gjose9 <gjose9@hotmail.com>
2019-12-30 00:36:37 +0800

    new test file

---
+++ test.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  test
+ *
+ *        Version:  1.0
+ *        Created:  Isnin 30 Dis 2019 12:19:04  +08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  pappu (ponnu), pappu@ponnu.com
+ *        Company:  Pappu Ponnu Inc
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+void main()
+{
+    printf ("test \n");
+}
+
